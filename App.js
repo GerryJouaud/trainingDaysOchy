6745e299205b586400af7fcb2d1865c97c4cbe42
Gerry Jouaud <gerry.jouaud@gmail.com>
2024-07-10 22:39:42 +0200

    10/07/2024-22:37 - Utilisation du Hook 'useState' pour ajouter et retirer des jours de notre tableau, affichage des jours sélectionnés

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, Button} from 'react-native';
 import CheckBox from './components/CheckBox';
+import {useState} from 'react';
 
 function App() {
+  // 'useState' Hook, with an empty array as initial state
+  const [value, setValue] = useState([]);
   return (
     <View style={styles.container}>
       <Text style={styles.sectionTitle}> Your Goal</Text>
       <Text style={styles.sectionDescription}>How many training session ?</Text>
       <CheckBox
+        //List that will be mapped in component CheckBox
         daysOfTheWeek={[
-          {label: 'Monday'},
-          {label: 'Tuesday'},
-          {label: 'Wednesday'},
-          {label: 'Thursday'},
-          {label: 'Friday'},
-          {label: 'Saturday'},
-          {label: 'Sunday'},
-        ]}></CheckBox>
+          {label: 'Monday', value: 'Monday'},
+          {label: 'Tuesday', value: 'Tuesday'},
+          {label: 'Wednesday', value: 'Wednesday'},
+          {label: 'Thursday', value: 'Thursday'},
+          {label: 'Friday', value: 'Friday'},
+          {label: 'Saturday', value: 'Saturday'},
+          {label: 'Sunday', value: 'Sunday'},
+        ]}
+        // 'Use state' Hook to CheckBox component
+        checkedDay={value}
+        onChange={setValue}></CheckBox>
+      <Button
+        title={'Submit'}
+        onPress={() => {
+          // Display the selected days
+          alert(`Selected Days: ${value}`);
+        }}></Button>
     </View>
   );
 }
--- components/CheckBox.js
+++ components/CheckBox.js
@@ -1,10 +1,38 @@
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-const CheckBox = ({daysOfTheWeek}) => {
+const CheckBox = ({daysOfTheWeek, checkedDay, onChange}) => {
+  // copy of checkedDay to manipulate easier
+  let daysSelected = [...checkedDay];
+
   return (
     <View style={[styles.container]}>
-      {daysOfTheWeek.map(day => {
-        return <Text style={styles.checkBox}>{day.label}</Text>;
+      {/*iteration of every day*/}
+      {daysOfTheWeek.map((day, index) => {
+        // check if the day have been selected
+        let active = daysSelected.includes(day.value);
+        return (
+          <TouchableOpacity
+            // ternary that define the style of the component if it's checked or not
+            style={active ? [styles.activeCheckBox] : styles.checkBox}
+            key={index}
+            onPress={() => {
+              // if already selected, day is filtered out of daysSelected array
+              if (active) {
+                daysSelected = daysSelected.filter(
+                  dayFiltered => dayFiltered !== day.value,
+                );
+
+                // re-render the array
+                return onChange(daysSelected);
+              }
+              // if it wasn't selected, add the day in the array
+              daysSelected.push(day.value);
+              // re-render the array
+              onChange(daysSelected);
+            }}>
+            <Text key={index}>{day.label}</Text>
+          </TouchableOpacity>
+        );
       })}
     </View>
   );
@@ -15,6 +43,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 2,
     padding: 1,
     backgroundColor: '#DDDDDD',
+    borderRadius: 3,
+  },
+  activeCheckBox: {
+    marginHorizontal: 2,
+    padding: 1,
+    backgroundColor: '#06b6d4',
     borderRadius: 3,
   },
   container: {
